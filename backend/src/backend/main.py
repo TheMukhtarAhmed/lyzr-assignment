0acb8478d44162e005a70fe1636e9e026458200d
Mukhtar Ahmed <itssmukhtar@gmail.com>
2025-10-28 07:15:13 +0530

    Update main.py

--- backend/src/backend/main.py
+++ backend/src/backend/main.py
@@ -28,7 +28,7 @@ manager = ConnectionManager()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://mukhtarahmed.com"],
+    allow_origins=["http://localhost:3000", "https://frontend.mukhtarahmed.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
